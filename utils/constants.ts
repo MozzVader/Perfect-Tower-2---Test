08fe84dc488db056873985777b49e6005dac41db
MozzVader <mozz05@gmail.com>
2026-03-05 18:18:05 -0300

    Add files via upload

---
+++ utils/constants.ts
@@ -0,0 +1,9 @@
+export const codeSample = `function greet(name: string): string {
+  return \`Hello, \${name}!\`;
+}
+
+const message: string = greet("World");
+console.log(message);
+
+// This is a sample code snippet
+// demonstrating TypeScript syntax`;
